cbec85476476da62a6170cc33abc5dcc6b15f137
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-17 22:47:23 +0300

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soru Numarasını Giriniz: ");
+            int qNum = int.Parse(Console.ReadLine());
+            if (qNum == 1)
+            {
+                // 1. dizi tanimla (sayi dizisi)
+                // 2. dizideki en buyugu bul ve indexini tut, maxNum
+                // 3. dizideki en kucugu bul ve indexini tut, minNum
+                // 4. minNum ile maxNum dizide yerini degistir
+
+                int[] numbers = {15, -68, 4582, 0, 62, 14, -2, -652};
+                int maxNum = numbers[0];
+                int minNum = numbers[0];
+                int maxIndex = 0, minIndex = 0, tempMin = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] > maxNum)
+                    {
+                        maxNum = numbers[i];
+                        maxIndex = i;
+                    }
+                    if (numbers[i] < minNum)
+                    {
+                        minNum = numbers[i];
+                        minIndex = i;
+                    }
+                }
+                tempMin = numbers[minIndex];// -652
+                numbers[minIndex] = numbers[maxIndex];
+                numbers[maxIndex] = tempMin;
+                foreach (var num in numbers)
+                {
+                    Console.Write(num + " ");
+                }
+
+                Console.Read();
+            }
+        }
+    }
+}
